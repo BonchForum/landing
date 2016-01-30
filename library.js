b1b0003cdc2abf04f471725ed34d330a7997f1cf
luchanso <oleg@loutchansky.com>
2016-01-30 21:08:01 +0300

    Fix rendering and remove trash

--- library.js
+++ library.js
@@ -5,8 +5,13 @@
 
   Landing.init = function(params, callback) {
     var app = params.router;
+    
+    app.get('/promo',function(req, res) {
+      res.sendFile(__dirname + '/index.html');
+    });
 
-    app.use('/promo', express.static(__dirname + '/'));
+    app.use('/promo', express.static(__dirname + '/assets'));
+    app.use('/promo', express.static(__dirname + '/images'));
   }
 
   module.exports = Landing;
